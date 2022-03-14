c050049658d0434ce8f267e400801e30167845be
Jeffry Gonzalez <Jeff@hypertheory.com>
2022-03-14 14:55:23 -0400

    sales tax thing

---
+++ ECommerce/SalesTaxCalculator.cs
@@ -0,0 +1,21 @@
+﻿namespace ECommerce
+{
+    public class SalesTaxCalculator
+    {
+        public SalesTaxCalculator()
+        {
+        }
+
+        public decimal CalculateSalesTaxOnAmount(decimal amountOfSale, string zipCode)
+        {
+           if(zipCode == "44107")
+            {
+                return amountOfSale * .07M;
+            } else
+            {
+                return amountOfSale * .10M;
+            }
+            
+        }
+    }
+}
